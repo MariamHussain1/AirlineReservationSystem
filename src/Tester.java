77f1cc0b881c05773970a955d49483694d17996c
Manal Hussain <mariam.mariamhussain@student.tdsb.on.ca>
2022-06-06 19:08:44 -0400

    added title page with a title, a next button and a plane image

--- src/Tester.java
+++ src/Tester.java
@@ -1,18 +1,11 @@
-
+import java.net.MalformedURLException;
 public class Tester {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//hello
-		System.out.println("bananas");
-
-		System.out.println("Helo My name is Shruthi ");
+	public static void main(String[] args) throws MalformedURLException {
 		
-		System.out.println("My name is Mariam");
-
-
-// hello 
-
+		TitlePage titlePage = new TitlePage();
+		
+		
 	}
 
 }
