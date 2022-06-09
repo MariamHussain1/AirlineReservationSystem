cabc47f6e57b6a02d0d6753e6a082d4ef84300bb
MariamHussain1 <84603135+MariamHussain1@users.noreply.github.com>
2022-06-09 09:43:02 -0400

    Edited Plane.java

--- src/Plane.java
+++ src/Plane.java
@@ -1,7 +1,36 @@
 
-public class Plane {
-  
-  public static final String FROM = "CA, Toronto";
+
+public abstract class Plane {
+	
+	private String from;
 	private String destination;
+	private String time;
+	private String flightType;
+	private String flightClass;
+	
+	public Plane(String time, String from, String destination, String flightType, String flightClass) {
+		this.time = time;
+		this.from = "CA, Toronto";
+		this.destination = destination;
+		this.flightType = flightType;
+		this.flightClass = flightClass;
+		
+	}
+	
+	public void setClass(String flight){
+		
+		flightClass = flight;
+	}
+	
+	public void setType(String type) {
+		
+		flightType = type;
+	}
+	
+	public void setDestination(String d) {
+		destination = d;
+	}
+	
+	public abstract double getPrice();
 
 }
