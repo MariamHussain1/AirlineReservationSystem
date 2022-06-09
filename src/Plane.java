e3d8b12b5faed5132716fbbd2700f969b23b5089
MariamHussain1 <84603135+MariamHussain1@users.noreply.github.com>
2022-06-09 09:58:07 -0400

    Update Plane.java

--- src/Plane.java
+++ src/Plane.java
@@ -2,35 +2,21 @@
 
 public abstract class Plane {
 	
-	private String from;
-	private String destination;
-	private String time;
-	private String flightType;
-	private String flightClass;
+	public static String from;
+	public static String destination;
+	public static String time;
+	public static String flightType;
+	public static String flightClass;
 	
 	public Plane(String time, String from, String destination, String flightType, String flightClass) {
-		this.time = time;
-		this.from = "CA, Toronto";
-		this.destination = destination;
-		this.flightType = flightType;
-		this.flightClass = flightClass;
+		Plane.time = time;
+		Plane.from = "CA, Toronto";
+		Plane.destination = destination;
+		Plane.flightType = flightType;
+		Plane.flightClass = flightClass;
 		
 	}
 	
-	public void setClass(String flight){
-		
-		flightClass = flight;
-	}
 	
-	public void setType(String type) {
-		
-		flightType = type;
-	}
-	
-	public void setDestination(String d) {
-		destination = d;
-	}
-	
-	public abstract double getPrice();
 
 }
