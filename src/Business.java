1b59c52ee5b125d22caca4922ba366a13f59b331
MariamHussain1 <84603135+MariamHussain1@users.noreply.github.com>
2022-06-13 09:39:11 -0400

    Update Business.java

--- src/Business.java
+++ src/Business.java
@@ -1,4 +1,43 @@
 
-public class Business {
+public class Business extends Plane {
+	
+	private double price;
+	private boolean under12 = false;
+	public Business(String time, String from, String destination, String flightType, String flightClass, double price) {
+		super(time, from, destination, flightType, flightClass);
+		this.price = price;
+			
+		}
+	
+	public Business(double price){
+    
+        this(time, from, destination, flightType, "Business", price);
+    }
+	
+	public String getDestination() {
+		return destination;
+	}
+	
+	public void setPrice(double price) {
+		if(flightType.equals("Domestic")) {
+			price = 2000;
+			if(under12 == true){
+				price = 1400;
+			}
+		}
+		
+		else if(flightType.equals("International")){
+			price = 3000;
+			if(under12 == true){
+				price = 2400;
+			}
+		}
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	
 
 }
