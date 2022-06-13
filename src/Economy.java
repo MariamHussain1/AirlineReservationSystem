093f06a934b17a2d66563e109136734c34fc4baa
MariamHussain1 <84603135+MariamHussain1@users.noreply.github.com>
2022-06-13 09:37:55 -0400

    Update Economy.java

--- src/Economy.java
+++ src/Economy.java
@@ -1,4 +1,43 @@
 
-public class Economy {
+public class Economy extends Plane {
+	
+	private double price;
+	private boolean under12 = false;
+	public Economy(String time, String from, String destination, String flightType, String flightClass, double price) {
+		super(time, from, destination, flightType, flightClass);
+		this.price = price;
+			
+		}
+	
+	public Economy(double price){
+    
+        this(time, from, destination, flightType, "Economy", price);
+    }
+	
+	public String getDestination() {
+		return destination;
+	}
+	
+	public void setPrice(double price) {
+		if(flightType.equals("Domestic")) {
+			price = 200;
+			if(under12 == true){
+				price = 100;
+			}
+		}
+		
+		else if(flightType.equals("International")){
+			price = 1500;
+			if(under12 == true){
+				price = 1000;
+			}
+		}
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	
 
 }
