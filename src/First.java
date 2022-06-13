466b42e16b6878e067a0fce4c66a15e14a7b155e
MariamHussain1 <84603135+MariamHussain1@users.noreply.github.com>
2022-06-13 09:35:38 -0400

    Update First.java

--- src/First.java
+++ src/First.java
@@ -2,6 +2,7 @@
 public class First extends Plane {
 	
 	private double price;
+	private boolean under12 = false;
 	public First(String time, String from, String destination, String flightType, String flightClass, double price) {
 		super(time, from, destination, flightType, flightClass);
 		this.price = price;
@@ -19,11 +20,22 @@ public class First extends Plane {
 	
 	public void setPrice(double price) {
 		if(flightType.equals("Domestic")) {
-			price = 4200; 
-			
+			price = 4200;
+			if(under12 == true){
+				price = 3600;
+			}
 		}
 		
-		else if(fligh)
+		else if(flightType.equals("International")){
+			price = 5800;
+			if(under12 == true){
+				price = 5000;
+			}
+		}
+	}
+	
+	public double getPrice(){
+		return price;
 	}
 	
 	
