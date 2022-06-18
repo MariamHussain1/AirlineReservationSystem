0f689fd3f0ccce29516b2c2c0f57a5527b5a9925
shrukon <102598913+shrukon@users.noreply.github.com>
2022-06-18 14:10:54 -0400

    changed

--- src/Economy.java
+++ src/Economy.java
@@ -1,39 +1,31 @@
 public class Economy extends Plane {
 	
 	public double price;
-	public double childPrice;
-	private int seats;
+	private double regPrice; 
+	private double childPrice; 
+	private int adultSeats;
 	private int childSeats;
 	public Economy(String time, String from, String destination, String flightType, String flightClass, int seats, int childSeats) {
 		super(time, from, destination, flightType, flightClass);
-		setPrice1(seats, childSeats); 	
 		}
 	
 	
-	public void setPrice1(int seats, int childSeats) {
+
+	
+	public double getPrice(int adultSeats, int childSeats){	
 		if(getFlightType().equals("Domestic")) {
-			price = 2000*seats;
-			System.out.println(price);
+			regPrice = 200;
+			childPrice = 100;
+						
 			
-			System.out.println("here");
 		}
 		else if(getFlightType().equals("International")){
-			price = 3000*seats;
+			regPrice = 1500;
+			childPrice = 1000;		
 		}
-			
-			
-		}
-	public void setPrice(double childPrice){
-		if(getFlightType().equals("Domestic")) {
-			childPrice = 1400;
-		}
-		else if(getFlightType().equals("International")){
-			price = 2400;
-		}
-		
-	}
-	public double getPrice(){	
-		return childSeats*childPrice + (seats-childSeats)*price;
+		//price = childSeats*childPrice + (seats-childSeats)*regPrice; 
+		return (childSeats*childPrice) + (adultSeats*regPrice) ;
+
 	}
 	
 	
