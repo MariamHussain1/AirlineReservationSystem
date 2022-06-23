0701fe55994773b3bb7694278e31ae37b9610700
shrukon <102598913+shrukon@users.noreply.github.com>
2022-06-22 22:25:54 -0400

    changed entirety

--- src/Business.java
+++ src/Business.java
@@ -1,39 +1,29 @@
 public class Business extends Plane {
 	
 	public double price;
-	public double childPrice;
-	private int seats;
+	private double regPrice; 
+	private double childPrice; 
+	private int adultSeats;
 	private int childSeats;
 	public Business(String time, String from, String destination, String flightType, String flightClass, int seats, int childSeats) {
 		super(time, from, destination, flightType, flightClass);
-		setPrice1(seats, childSeats); 	
 		}
 	
 	
-	public void setPrice1(int seats, int childSeats) {
+	public double getPrice(int adultSeats, int childSeats) {
 		if(getFlightType().equals("Domestic")) {
-			price = 2000*seats;
-			System.out.println(price);
-			
-			System.out.println("here");
-		}
-		else if(getFlightType().equals("International")){
-			price = 3000*seats;
-		}
-			
-			
-		}
-	public void setPrice(double childPrice){
-		if(getFlightType().equals("Domestic")) {
+			regPrice = 2000;
 			childPrice = 1400;
-		}
+
+					}
 		else if(getFlightType().equals("International")){
-			price = 2400;
+			regPrice = 3000;
+			childPrice = 2400;
+
 		}
-		
-	}
-	public double getPrice(){	
-		return childSeats*childPrice + (seats-childSeats)*price;
+			
+
+		return childSeats*childPrice + adultSeats*price;
 	}
 	
 	
